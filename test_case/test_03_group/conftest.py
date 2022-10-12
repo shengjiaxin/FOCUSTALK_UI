827a0d17bd8aea66d631fcee8e41ab99b4a597c4
unknown <shengjiaxin@focuschina.com>
2022-10-12 16:28:38 +0800

    提交

---
+++ test_case/test_03_group/conftest.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# Author：谌雪晴
+
+import pytest
+from appium import webdriver
+from Common.publicMethod import PubMethod
+
+
+# 启动APP
+@pytest.fixture(scope="function")
+def start_app(android_setting):
+    global driver
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", android_setting)
+    return PubMethod.is_login(driver, 2)
+
+
+# 关闭APP
+@pytest.fixture()
+def reload_app():
+    yield driver
+    driver.quit()
